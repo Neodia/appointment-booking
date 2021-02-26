998f73c2091275d503ea4422a8f75f8774f26996
ricardo.nunesdeo <ricardo.nunes-de-oliveira@etu.hesge.ch>
2021-02-26 20:57:17 +0100

    Changed appointment class to be the same as backend.

--- appointment-booking-frontend/src/app/appointment.ts
+++ appointment-booking-frontend/src/app/appointment.ts
@@ -1,17 +1,14 @@
-import { Time } from "@angular/common";
 import { BookingInfo } from "./booking-info";
 
 export class Appointment {
     id: number;
     type: string;
     date: Date;
-    time: Time;
     info: BookingInfo;
 
-    constructor(type: string, date: Date, time: Time, info: BookingInfo) {
+    constructor(type: string, date: Date, info: BookingInfo) {
         this.type = type;
         this.date = date;
-        this.time = time;
         this.info = info;
     }
 }
