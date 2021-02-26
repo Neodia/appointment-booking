db96950f700ba616e9cea92d38bca766171b6e4a
ricardo.nunesdeo <ricardo.nunes-de-oliveira@etu.hesge.ch>
2021-02-26 20:55:40 +0100

    Added cross origins.

--- src/main/java/neo/dia/appointmentbooking/controllers/ScheduleController.java
+++ src/main/java/neo/dia/appointmentbooking/controllers/ScheduleController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import neo.dia.appointmentbooking.entities.Appointment;
 import neo.dia.appointmentbooking.repositories.ScheduleRepository;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class ScheduleController {
 
     @Autowired
