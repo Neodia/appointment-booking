43b8a3f4c67ceabc00da1878ce2c46153aad24a4
ricardo.nunesdeo <ricardo.nunes-de-oliveira@etu.hesge.ch>
2021-02-27 21:10:02 +0100

    Added more values to database initially.

--- src/main/java/neo/dia/appointmentbooking/AppointmentBookingApplication.java
+++ src/main/java/neo/dia/appointmentbooking/AppointmentBookingApplication.java
@@ -27,11 +27,25 @@ public class AppointmentBookingApplication {
 
 			schedule.save( new Appointment(
 				AppointmentType.First, 
-				LocalDateTime.of(2021, 2, 26, 14, 0),
-				new BookingInfo("Ricardo", "Oiveira", LocalDate.of(1999, 5, 10), "0788677270", "mail@mail.com", "Salut")) );
+				LocalDateTime.of(2021, 3, 1, 9, 30),
+				new BookingInfo("Ricardo", "Oiveira", LocalDate.of(1999, 5, 10), "078", "mail@mail.com", "Salut")) );
 
+			schedule.save( new Appointment(
+				AppointmentType.First, 
+				LocalDateTime.of(2021, 3, 1, 14, 0),
+				new BookingInfo("Marcus", "Aurelius", LocalDate.of(1999, 5, 10), "078", "mail@mail.com", "Salut")) );
+
+			schedule.save( new Appointment(
+				AppointmentType.FollowUp, 
+				LocalDateTime.of(2021, 3, 3, 10, 0),
+				new BookingInfo("Tim", "Ferris", LocalDate.of(1999, 5, 10), "078", "mail@mail.com", "Salut")) );
+
+			schedule.save( new Appointment(
+				AppointmentType.First, 
+				LocalDateTime.of(2021, 3, 5, 13, 0),
+				new BookingInfo("Matt", "D'Avella", LocalDate.of(1999, 5, 10), "0788677270", "mail@mail.com", "Salut")) );
+			
 			System.out.println("Après avoir peuplé : " + schedule.findAll());
-
 		};
 	}
 }
