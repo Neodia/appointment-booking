59a761aea81335cecb0c9a85127ce4eb077c2cd4
ricardo.nunesdeo <ricardo.nunes-de-oliveira@etu.hesge.ch>
2021-02-28 18:05:28 +0100

    Added time period class.

---
+++ appointment-booking-frontend/src/app/time-period.ts
@@ -0,0 +1,9 @@
+export class TimePeriod {
+    startTime : Date;
+    endTime : Date;
+
+	constructor(startTime : Date, endTime: Date) {
+        this.startTime = startTime;
+		this.endTime = endTime;
+	}
+}
